99e1c539aebd7a9e54b4176d86f9ac235ffb0a62
Benben123 <benzhang.bupt@gmail.com>
2020-04-12 13:24:49 +0800

    correct question

--- myAlgorithm/py/ThreeSums.py
+++ myAlgorithm/py/ThreeSums.py
@@ -1,43 +1,20 @@
 # 3 sum
 """
-704. Binary Search
-Easy
-
-524
-
-42
-
-Add to List
-
-Share
-Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
-
-
-Example 1:
-
-Input: nums = [-1,0,3,5,9,12], target = 9
-Output: 4
-Explanation: 9 exists in nums and its index is 4
-
-Example 2:
-
-Input: nums = [-1,0,3,5,9,12], target = 2
-Output: -1
-Explanation: 2 does not exist in nums so return -1
-
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
 Note:
 
-You may assume that all elements in nums are unique.
-n will be in the range [1, 10000].
-The value of each element in nums will be in the range [-9999, 9999].
+The solution set must not contain duplicate triplets.
 
+Example:
 
-idea:
-1
-2
-3
+Given array nums = [-1, 0, 1, 2, -1, -4],
 
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
 """
 
 
