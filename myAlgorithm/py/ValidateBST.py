14e63c2104e77762575a188d6e4b2c5879e627ec
Benben123 <benzhang.bupt@gmail.com>
2020-04-19 17:11:42 +0800

    validate BST

---
+++ myAlgorithm/py/ValidateBST.py
@@ -0,0 +1,64 @@
+"""
+Validate Binary Search Tree
+Medium
+
+3334
+
+473
+
+Add to List
+
+Share
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+
+Example 1:
+
+    2
+   / \
+  1   3
+
+Input: [2,1,3]
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+
+Input: [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.help(root, -float('inf'), float('inf'))
+
+    def help(self, root, low, high):
+        if root is None:
+            return True
+        if not low < root.val < high:
+            return False
+        return self.help(root.left, low, min(high, root.val)) and self.help(root.right, max(low, root.val), high)
+
