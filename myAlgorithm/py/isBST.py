fb61f04771a15f39d055e785b4ad1c9ce6f702bb
Benben123 <benzhang.bupt@gmail.com>
2020-04-29 08:11:39 +0800

    check if it's valid BST

---
+++ myAlgorithm/py/isBST.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isBST(self, root):
+        return self.help(root, -float('inf'), float('inf'))
+
+    def help(self, root, low, high):
+        if root is None:
+            return True
+        return root.val < high and root.val > low \
+               and self.help(root.left, low, root.val) \
+               and self.help(root.right, root.val, hight)
