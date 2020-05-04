2fedd946378d1a2f221dd771e9adf1ff681b90c8
Benben123 <benzhang.bupt@gmail.com>
2020-05-04 17:17:47 +0800

    tell if BST via none recursive method, the idea is similar to other inorder traverse

---
+++ myAlgorithm/py/BinaryTree/IsBSTNotRecur.py
@@ -0,0 +1,48 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: True if the binary tree is BST, or false
+    """
+
+    def isValidBST(self, root):
+        if root is None:
+            return True
+
+        stack = []
+        while root:
+            stack.append(root)
+            root = root.left
+
+        last_node = stack[-1]
+        while stack:
+            node = stack.pop()
+            if node.right:
+                node = node.right
+                while node:
+                    stack.append(node)
+                    node = node.left
+
+            # the only difference compare to an inorder traversal iteration
+            # this problem disallowed equal values so it's <= not <
+            # Because inorder traverse BST must be ascending numbers, so the 'last_node' must be smaller than current stack[-1]
+            
+            if stack:
+                if stack[-1].val <= last_node.val:
+                    return False
+                last_node = stack[-1]
+        return True
+
+if __name__ == "__main__":
+    root = TreeNode(10)
+    root.left = TreeNode(5)
+    root.right = TreeNode(20)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(8)
+    root.right.left = TreeNode(12)
+    test = Solution().isValidBST(root)
+    print test
