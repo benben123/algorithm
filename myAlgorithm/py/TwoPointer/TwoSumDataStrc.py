df200770a03f37dae3acee64631d2d20b659eadd
Benben123 <benzhang.bupt@gmail.com>
2020-05-19 00:49:56 +0800

    add comments

--- myAlgorithm/py/TwoPointer/TwoSumDataStrc.py
+++ myAlgorithm/py/TwoPointer/TwoSumDataStrc.py
@@ -1,3 +1,21 @@
+
+"""
+Description
+
+English
+Design and implement a TwoSum class. It should support the following operations: add and find.
+
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+Have you met this question in a real interview?  Yes
+Problem Correction
+Example
+Example 1:
+
+add(1); add(3); add(5);
+find(4) // return true
+find(7) // return false
+"""
 class TwoSum:
     def __init__(self):
         self.store =[]
--- myAlgorithm/py/TwoPointer/UniqPair.py
+++ myAlgorithm/py/TwoPointer/UniqPair.py
@@ -1,3 +1,28 @@
+"""
+Two Sum - Unique pairs
+Description
+
+English
+Given an array of integers, find how many unique pairs in the array such that their sum is equal to a specific target number. Please return the number of pairs.
+Have you met this question in a real interview?  Yes
+Problem Correction
+Example
+Example 1:
+
+Input: nums = [1,1,2,45,46,46], target = 47
+Output: 2
+Explanation:
+
+1 + 46 = 47
+2 + 45 = 47
+
+Example 2:
+
+Input: nums = [1,1], target = 2
+Output: 1
+Explanation:
+1 + 1 = 2
+"""
 class UniqPair:
     def uniqPair(self, lst, tar):
         lst.sort()
