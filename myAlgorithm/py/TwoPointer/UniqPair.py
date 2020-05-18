5ffa26b2e1e65e861459da2ab5d8cd8175ad5317
Benben123 <benzhang.bupt@gmail.com>
2020-05-19 00:46:43 +0800

    two pointers

---
+++ myAlgorithm/py/TwoPointer/UniqPair.py
@@ -0,0 +1,19 @@
+class UniqPair:
+    def uniqPair(self, lst, tar):
+        lst.sort()
+        left, right = 0, len(lst) - 1
+        count = 0
+        while left < right:
+            if lst[left] + lst[right] >tar:
+                right -= 1
+            elif lst[left] + lst[right] <tar:
+                left += 1
+            else:
+                count += 1
+                left += 1
+                right -= 1
+                while left < right and lst[left] == lst[left-1]:
+                    left += 1
+                while left < right and lst[right] == lst[right + 1]:
+                    right -= 1
+        return count 
